6baaf61297cd8c16700e88b9357638dc61cf415a
Tranngoclinh <nguyenlinh23444@gmail.com>
2022-07-21 10:43:08 +0700

    update-address-customer

--- HotelBooking/database/migrations/2022_07_19_044901_create_customers_table.php
+++ HotelBooking/database/migrations/2022_07_19_044901_create_customers_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('email');
             $table->string('phone');
-            $table->string('address');
+            $table->string('address')->nullable();
             $table->string('cmnd');
             $table->timestamps();
             $table->softDeletes();
