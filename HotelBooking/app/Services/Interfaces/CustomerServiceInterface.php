790954a37e910032f2b952f4834873bc348002dc
Tranngoclinh <nguyenlinh23444@gmail.com>
2022-07-22 11:33:12 +0700

    update_home

--- HotelBooking/app/Services/Interfaces/CustomerServiceInterface.php
+++ HotelBooking/app/Services/Interfaces/CustomerServiceInterface.php
@@ -2,5 +2,7 @@
 namespace App\Services\Interfaces;
 
 interface CustomerServiceInterface extends Service{
-
+   public function trashedItems();
+   public function restore($id);
+   public function force_destroy($id);
 }
