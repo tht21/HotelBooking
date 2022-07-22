42be35403b22ff3ece83e1f0372ead56dd4edd5d
Tranngoclinh <nguyenlinh23444@gmail.com>
2022-07-22 09:25:14 +0700

    update_user

--- HotelBooking/app/Repositories/Interfaces/UserInterface.php
+++ HotelBooking/app/Repositories/Interfaces/UserInterface.php
@@ -7,5 +7,7 @@ use App\Repositories\Interfaces\RepositoryInterface;
 
 interface UserInterface extends RepositoryInterface
 {
-
+   public function trashedItems();
+   public function restore($id);
+   public function force_destroy($id);
 }
