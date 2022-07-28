80a473ffc1a9bd035458cef0c80b2a847182b74c
tht2 <quocvietk5959@gmail.com>
2022-07-28 09:18:48 +0700

    update-delete-bookroom-

--- HotelBooking/resources/views/Layouts/admin/includes/sidebar.blade.php
+++ HotelBooking/resources/views/Layouts/admin/includes/sidebar.blade.php
@@ -1,12 +1,13 @@
 <div class="sidebar sidebar-style-2">
-   <div class="sidebar-wrapper scrollbar scrollbar-inner">
-      <div class="sidebar-content">
-         <div class="user">
-            <div class="avatar-sm float-left mr-2">
-               <img src="{{asset($profile_user->avatar)}}" width="100px" height="100px" alt="..." class="avatar-img rounded-circle">
-            </div>
-            <div class="info">
-               <a data-toggle="collapse" href="#collapseExample" aria-expanded="true">
+    <div class="sidebar-wrapper scrollbar scrollbar-inner">
+        <div class="sidebar-content">
+            <div class="user">
+                <div class="avatar-sm float-left mr-2">
+                    <img src="{{asset($profile_user->avatar)}}" width="100px" height="100px" alt="..."
+                         class="avatar-img rounded-circle">
+                </div>
+                <div class="info">
+                    <a data-toggle="collapse" href="#collapseExample" aria-expanded="true">
 
                   <span>
                       {{$profile_user->name }}
@@ -32,14 +33,14 @@
                     </div>
                 </div>
             </div>
-         <ul class="nav nav-primary">
-            <li class="nav-item active">
-               <a href="{{route('home')}}">
-                  <i class="fas fa-home"></i>
-                  <p>Thống Kê</p>
-               </a>
-            </li>
-            <li class="nav-section">
+            <ul class="nav nav-primary">
+                <li class="nav-item active">
+                    <a href="{{route('home')}}">
+                        <i class="fas fa-home"></i>
+                        <p>Thống Kê</p>
+                    </a>
+                </li>
+                <li class="nav-section">
 
                <span class="sidebar-mini-icon">
                   <i class="fa fa-ellipsis-h"></i>
@@ -54,30 +55,30 @@
                     <div class="collapse" id="base">
                         <ul class="nav nav-collapse">
                             @if(Auth::user()->hasPermission('Bookings_viewAny'))
-                            
-                            <li>
-                                <a href="{{route('bookingrooms.index')}}">
-                                    <span class="sub-item">Xem Dạng Danh Sách</span>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="{{route('bookingrooms.list')}}">
-                                    <span class="sub-item">Danh Sách đặt phòng</span>
-                                </a>
-                            </li>
+
+                                <li>
+                                    <a href="{{route('bookingrooms.index')}}">
+                                        <span class="sub-item">Xem Dạng Danh Sách</span>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="{{route('bookingrooms.list')}}">
+                                        <span class="sub-item">Danh Sách đặt phòng</span>
+                                    </a>
+                                </li>
                             @endif
                         </ul>
                     </div>
                 </li>
-            @if(Auth::user()->hasPermission('Customers_viewAny'))
-             <li class="nav-item">
-                 <a href="{{route('customers.index')}}">
-                     <i class="fas fa-th-list"></i>
-                     <p>Khách Hàng</p>
-                 </a>
-             </li>
-             @endif
-             <li class="nav-item">
+                @if(Auth::user()->hasPermission('Customers_viewAny'))
+                    <li class="nav-item">
+                        <a href="{{route('customers.index')}}">
+                            <i class="fas fa-th-list"></i>
+                            <p>Khách Hàng</p>
+                        </a>
+                    </li>
+                @endif
+                <li class="nav-item">
                     <a data-toggle="collapse" href="#tables">
                         <i class="fas fa-table"></i>
                         <p>Quản Lý Phòng</p>
@@ -86,48 +87,48 @@
                     <div class="collapse" id="tables">
                         <ul class="nav nav-collapse">
                             @if(Auth::user()->hasPermission('RoomType_viewAny'))
-                            <li>
-                                <a href="{{route('roomtype.index')}}">
-                                    <span class="sub-item">Loại Phòng</span>
-                                </a>
-                            </li>
+                                <li>
+                                    <a href="{{route('roomtype.index')}}">
+                                        <span class="sub-item">Loại Phòng</span>
+                                    </a>
+                                </li>
                             @endif
                             @if(Auth::user()->hasPermission('Rooms_viewAny'))
-                            <li>
-                                <a href="{{route('rooms.index')}}">
-                                    <span class="sub-item">Phòng</span>
-                                </a>
-                            </li>
+                                <li>
+                                    <a href="{{route('rooms.index')}}">
+                                        <span class="sub-item">Phòng</span>
+                                    </a>
+                                </li>
                             @endif
                         </ul>
                     </div>
-             </li>
-             @if(Auth::user()->hasPermission('User_viewAny'))
-                 <li class="nav-item">
-                     <a href="{{route('users.index')}}">
-                         <i class="fas fa-user"></i>
-                         <p>Nhân Viên</p>
-                     </a>
-                 </li>
-             @endif
-             @if(Auth::user()->hasPermission('UserGroup_viewAny'))
-                 <li class="nav-item">
-                     <a href="{{route('usergroups.index')}}">
-                         <i class="fas fa-users"></i>
-                         <p>Nhóm Nhân Viên</p>
-                     </a>
-                 </li>
-             @endif
+                </li>
+                @if(Auth::user()->hasPermission('User_viewAny'))
+                    <li class="nav-item">
+                        <a href="{{route('users.index')}}">
+                            <i class="fas fa-user"></i>
+                            <p>Nhân Viên</p>
+                        </a>
+                    </li>
+                @endif
+                @if(Auth::user()->hasPermission('UserGroup_viewAny'))
+                    <li class="nav-item">
+                        <a href="{{route('usergroups.index')}}">
+                            <i class="fas fa-users"></i>
+                            <p>Nhóm Nhân Viên</p>
+                        </a>
+                    </li>
+                @endif
 
-             <li class="nav-item">
-                 <a href="#forms">
-                     <i class="fas fa-list"></i>
-                     <p>Menu</p>
-                 </a>
-             </li>
-             <li class="nav-item">
-                 <a href="#maps">
-                     <i class="fas fa-phone"></i>
+                <li class="nav-item">
+                    <a href="#forms">
+                        <i class="fas fa-list"></i>
+                        <p>Menu</p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="#maps">
+                        <i class="fas fa-phone"></i>
                         <p>Liên Hệ</p>
                     </a>
                 </li>
